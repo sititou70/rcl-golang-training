7d07ee1a403d7b0b67c87d8ae786931f42d79c79
Takuya Shizukuishi <sititou70@gmail.com>
2021-11-03 18:35:23 +0900

    feat: ~ ex12.2

---
+++ ch12/ex12.2/display.go
@@ -0,0 +1,100 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 333.
+
+// Package display provides a means to display structured data.
+package display
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+//!+Display
+
+func Display(name string, x interface{}, limit int) {
+	fmt.Printf("Display %s (%T):\n", name, x)
+	b := strings.Builder{}
+	display(&b, name, reflect.ValueOf(x), 0, limit)
+	fmt.Print(b.String())
+}
+
+//!-Display
+
+// formatAtom formats a value without inspecting its internal structure.
+// It is a copy of the the function in gopl.io/ch11/format.
+func formatAtom(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Invalid:
+		return "invalid"
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(v.Uint(), 10)
+	// ...floating-point and complex cases omitted for brevity...
+	case reflect.Bool:
+		if v.Bool() {
+			return "true"
+		}
+		return "false"
+	case reflect.String:
+		return strconv.Quote(v.String())
+	case reflect.Chan, reflect.Func, reflect.Ptr,
+		reflect.Slice, reflect.Map:
+		return v.Type().String() + " 0x" +
+			strconv.FormatUint(uint64(v.Pointer()), 16)
+	default: // reflect.Array, reflect.Struct, reflect.Interface
+		return v.Type().String() + " value"
+	}
+}
+
+//!+display
+func display(b *strings.Builder, path string, v reflect.Value, depth int, limit int) {
+	if depth >= limit {
+		fmt.Fprintf(b, "%s = %s\n", path, formatAtom(v))
+		return
+	}
+
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Fprintf(b, "%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			display(b, fmt.Sprintf("%s[%d]", path, i), v.Index(i), depth+1, limit)
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(b, fieldPath, v.Field(i), depth+1, limit)
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			kb := &strings.Builder{}
+			display(kb, "key", key, depth+1, limit)
+
+			display(b, fmt.Sprintf("%s[%s]", path, strings.Trim(kb.String(), "\n")), v.MapIndex(key), depth+1, limit)
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			fmt.Fprintf(b, "%s = nil\n", path)
+		} else {
+			display(b, fmt.Sprintf("(*%s)", path), v.Elem(), depth+1, limit)
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Fprintf(b, "%s = nil\n", path)
+		} else {
+			fmt.Fprintf(b, "%s.type = %s\n", path, v.Elem().Type())
+			display(b, path+".value", v.Elem(), depth+1, limit)
+		}
+	default: // basic types, channels, funcs
+		fmt.Fprintf(b, "%s = %s\n", path, formatAtom(v))
+	}
+}
+
+//!-display
